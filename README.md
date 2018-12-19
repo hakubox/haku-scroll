479d708494f3d41597f011fec073421d23969c94
hakubox <lidian_226@163.com>
2018-12-20 01:28:26 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # haku-scroll
-vue滚动条组件
+
+> vue滚动条组件
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
+```
+
+For detailed explanation on how things work, consult the [docs for vue-loader](http://vuejs.github.io/vue-loader).
---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+      <haku-scroll ref="sc1" class="haku-scroll">
+          <p>-</p><p>-</p>
+          <p>-</p><p>-</p>
+          <p>-</p><p>-</p>
+          <p>-</p><p>-</p>
+          <p>-</p><p>-</p>
+          <p>-</p><p>-</p>
+          <p>-</p><p>-</p>
+          <p>-</p><p>-</p>
+      </haku-scroll>
+  </div>
+</template>
+
+<script>
+import hakuScroll from "./plugin/haku-scroll/haku-scroll";
+export default {
+    components: {
+        hakuScroll,
+    },
+    name: 'app',
+    data () {
+        return {
+            msg: 'Welcome to Your Vue.js App'
+        }
+    }
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+.haku-scroll {
+    background-color: #EEE;
+    width: 200px;
+    height: 200px;
+    margin: auto;
+}
+</style>
